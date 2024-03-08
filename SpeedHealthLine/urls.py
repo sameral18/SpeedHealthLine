bf17aa695703014430264509d56d3da665ee4eb7
sameral18 <120316812+sameral18@users.noreply.github.com>
2024-03-08 11:03:35 +0200

    Update urls.py

--- SpeedHealthLine/urls.py
+++ SpeedHealthLine/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for SpeedHealthLine project.
+URL configuration for SpeedHealthLine project
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
